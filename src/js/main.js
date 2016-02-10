4743044d284c35d54dea341c503d8ce87e2b5423
Vasilis Tsirimokos <v.tsirimokos@gmail.com>
2016-02-10 17:48:11 +0000

    Homepage structure

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,19 @@
-// src/js/magic.js
-
-var hello = 'look im grunting!'
-
-var awesome = 'yes it is awesome!'
+jQuery(document).ready(function($){
+	var isLateralNavAnimating = false;
+	
+	//open/close lateral navigation
+	$('.cd-nav-trigger').on('click', function(event){
+		event.preventDefault();
+		//stop if nav animation is running 
+		if( !isLateralNavAnimating ) {
+			if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true; 
+			
+			$('body').toggleClass('navigation-is-open');
+			$('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				//animation is over
+				isLateralNavAnimating = false;
+			});
+		}
+	});
+});
 
