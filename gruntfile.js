a3265c727ead4f9e0eb2d60f80041fb827375b19
Vasilis <vasileios.tsirimokos@aklamio.com>
2018-04-25 22:21:30 +0200

    Revert "Grunt-sass updates"
    This reverts commit 700caf533bedff1df03e0b0a0639ea7ff8b1c788.

--- gruntfile.js
+++ gruntfile.js
@@ -25,8 +25,8 @@ module.exports = function(grunt) {
 			dist: {	// Target
 	            // this is the "dev" Sass config used with "grunt watch" command
 	            options: {
-					style: 'compressed',
-					includePaths: [ 'node_modules/bootstrap-sass/assets/stylesheets' ]
+	                style: 'compressed',
+	                loadPath: 'node_modules/bootstrap-sass/assets/stylesheets'
 	            },
 	            files: {
 	                'dist/css/style.css': 'src/sass/style.scss'
@@ -157,7 +157,7 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-copy');
